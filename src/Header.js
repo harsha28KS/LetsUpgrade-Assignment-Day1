9f9cd3f0f824d195ced9a33b84b47872756fbc40
Harsha KS <71483257+harsha28KS@users.noreply.github.com>
2021-08-18 02:43:29 +0530

    DAY - 1 Assignment Submitted

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+function Header() {
+  return (
+    <div>
+      <h1>LetsUpgrade</h1>
+      <br />
+      <br />
+      <h2>Learn. Build. Share. Grow.</h2>
+
+      <p>
+        On a mission to Upskill 10 Million techies by the best industry experts,
+        for the 21st century skills with a collaborative community, to Upgrade
+        together.
+      </p>
+      <p>👨‍💼 Learn from 160+ Working professionals</p>
+      <p>🤑 Start learning for Free</p>
+      <p> ⚡ Extreme Affordability, exclusively for you</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default Header;
